c6c6cb2c0f38540f4a65f91f86cb6f32fe93f36a
Christian Estevam <christianestevamb@gmail.com>
2024-03-19 23:22:59 -0300

    imagens e herois

--- atividades/src/App.jsx
+++ atividades/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Pai from './components/atividade01/01Pai.jsx';
-import * as PC from './components/atividade01/02MeuPC.jsx';
-
+import PC from './components/atividade01/02MeuPC.jsx';
+import { Hero, Enemy, Arena, World } from './components/atividade01/03Batalha.jsx';
 
 const App = () => {
     return (
@@ -12,7 +12,15 @@ const App = () => {
             <PC.PlacaMae nome="Placa Mãe Raiz" preco="R$ 500" />
             <PC.Memoria nome="SSD 500 GB" preco="R$ 200" />
             <PC.PlacaDeVideo nome="Placa de Video RTX 3090" preco="R$ 700" />
+
+            <World>
+            <Arena arena="Cidade Republica – Underground Arena">
+                <Hero name="Korra" img="korra.png" />
+                <Enemy name="Amon Dobrador de Sague" img="amon.png" />
+            </Arena>
+        </World>
         </div>
+       
     );
 };
 
